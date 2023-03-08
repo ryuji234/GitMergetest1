e077fed200a0d11230c330f75a1f965450c6d8a7
ryuji234 <jihwanryu7@gmail.com>
2023-03-08 11:28:52 +0900

    [main] setup main project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GitMergeTest
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter*ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }   
+        }
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+        }
+
+    }
+
+}
+
