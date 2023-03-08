98cda6dcacbc4c0d228213b472b832ecca2c91f6
ryuji234 <jihwanryu7@gmail.com>
2023-03-08 11:38:48 +0900

    [Alpha]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,16 @@
-﻿namespace GitMergeTest
-{
-    internal class program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
-        }
-    }
+namespace GitMergeTest
+{
+    // { 2023. 03. 08. Ruler 클래스 코드 위치 변경 / Alpha
     public class Ruler
     {
         private const float ONE_INCH = 2.54f;
         public int Centimeter { get; set; } = 0;
         public float Inch
         {
-            get { return Centimeter*ONE_INCH; }
-            private set { Centimeter = (int)(value / ONE_INCH); }   
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
         }
         public Ruler(int cmValue) { Centimeter = cmValue; }
 
@@ -24,6 +20,16 @@
         }
 
     }
+    // } 2023. 03. 08. Ruler 클래스 코드 위치 변경 / Alpha
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+    
 
 }
 
